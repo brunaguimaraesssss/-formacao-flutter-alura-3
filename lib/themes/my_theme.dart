6de4366f2352c0e2f6584721bc0fe259a61ae2e1
Bruna Guimarães <bruna.guimaraes@qyon.com>
2023-02-02 14:51:55 -0300

    first commit

---
+++ lib/themes/my_theme.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:alubank/themes/themes_colors.dart';
+import 'package:flutter/material.dart';
+
+ThemeData MyTheme = ThemeData(
+  primarySwatch: ThemeColors.primaryColor,
+  primaryColor: ThemeColors.primaryColor,
+  brightness: Brightness.dark,
+  fontFamily: 'Raleway',
+  textTheme: const TextTheme(
+      bodyMedium: TextStyle(
+        fontSize: 16,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 28,
+        fontWeight: FontWeight.bold,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.normal,
+      )),
+);
